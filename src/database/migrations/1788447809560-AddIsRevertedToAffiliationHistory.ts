0457463dca70a478e51ab775c907e6736f973e1c
Alberto Domingo Basabe Acurero <alberto_12ba@hotmail.es>
2026-09-07 16:18:19 -0400

    refactor(contracts): remove unnecessary constraints and indexes in affiliation history migration

--- src/database/migrations/1788447809560-AddIsRevertedToAffiliationHistory.ts
+++ src/database/migrations/1788447809560-AddIsRevertedToAffiliationHistory.ts
@@ -5,11 +5,6 @@ export class AddIsRevertedToAffiliationHistory1788447809560 implements Migration
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "contract_persons" DROP CONSTRAINT "FK_contract_persons_plan"`,
-    );
-    await queryRunner.query(`DROP INDEX "public"."IDX_users_email"`);
-    await queryRunner.query(`DROP INDEX "public"."IDX_payments_status"`);
-    await queryRunner.query(
       `ALTER TABLE "affiliation_history" ADD "is_reverted" boolean NOT NULL DEFAULT false`,
     );
     await queryRunner.query(
@@ -21,12 +16,6 @@ export class AddIsRevertedToAffiliationHistory1788447809560 implements Migration
     await queryRunner.query(
       `UPDATE "affiliation_history" SET "is_reverted" = true WHERE "reason" LIKE 'REVERTIDO:%'`,
     );
-    await queryRunner.query(
-      `CREATE INDEX "IDX_payments_status_send_at" ON "payments" ("status", "send_at") `,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "contract_persons" ADD CONSTRAINT "FK_a31e2f22ff3f92d7e7064a41bf1" FOREIGN KEY ("plan_id") REFERENCES "plans"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
@@ -37,10 +26,5 @@ export class AddIsRevertedToAffiliationHistory1788447809560 implements Migration
     await queryRunner.query(`DROP INDEX "public"."IDX_ah_is_reverted"`);
     await queryRunner.query(`ALTER TABLE "affiliation_history" DROP COLUMN "reverted_at"`);
     await queryRunner.query(`ALTER TABLE "affiliation_history" DROP COLUMN "is_reverted"`);
-    await queryRunner.query(`CREATE INDEX "IDX_payments_status" ON "payments" ("status") `);
-    await queryRunner.query(`CREATE INDEX "IDX_users_email" ON "users" ("email") `);
-    await queryRunner.query(
-      `ALTER TABLE "contract_persons" ADD CONSTRAINT "FK_contract_persons_plan" FOREIGN KEY ("plan_id") REFERENCES "plans"("id") ON DELETE SET NULL ON UPDATE CASCADE`,
-    );
   }
 }
